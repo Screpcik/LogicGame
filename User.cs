fe71a31f15aff6c2ff75c4b5a84ef9d2d05acbaf
Łukasz Darmochwał <tcpvg@o2.pl>
2020-10-15 18:22:39 +0200

    Dodana klasa users oraz okienko MenuWindow

---
+++ User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace LogicGame
+{
+    public class User
+    {
+        public int Id;
+        public string UserName;
+        public string Password;
+        public User(int Id, string login, string password)
+        {
+            this.Id = Id;
+            this.UserName = login;
+            this.Password = password;
+        }
+        public bool checkPassword(string Password)
+        {
+            byte[] salat = Encoding.ASCII.GetBytes(Id + "LogicGames");
+            Rfc2898DeriveBytes pdkdf2 = new Rfc2898DeriveBytes(Password, salat);
+            Password = Encoding.ASCII.GetString(pdkdf2.GetBytes(20));
+            if (this.Password == Password) return true;
+            else return false;
+        }
+    }
+}
