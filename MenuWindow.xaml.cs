bf6a92a02cb1121752e02544ed5f6b25c50af3b1
Łukasz Darmochwał <tcpvg@o2.pl>
2020-10-16 17:17:59 +0200

    Dodanie okien poszczególnych gier

--- MenuWindow.xaml.cs
+++ MenuWindow.xaml.cs
@@ -17,13 +17,52 @@ namespace LogicGame
     /// <summary>
     /// Logika interakcji dla klasy MenuWindow.xaml
     /// </summary>
+    /// 1 - Działania Matematyczne
+    /// 2 - Zapamiętywanie
+    /// 3 - było nie było
+    /// 4 - czas reakcji
+    /// 5 - Zapamiętaj Numer
+    /// 6 - Od najmniejszego
+    /// 7 - Alfabet
     public partial class MenuWindow : Window
     {
         string login;
+        int gameNumber;
         public MenuWindow(string login)
         {
             this.login = login;
             InitializeComponent();
+        }
+        void ChooseGame()
+        {
+            switch (gameNumber)
+            {
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 6:
+
+                    break;
+
+            }
+                
         }
     }
 }
